2b1775ec3c8e5fec65b7679b2b91de80e709f1fd
ahmetikrdg <karadagahmetkaa@outlook.com>
2020-08-16 15:43:08 +0300

    first commit

---
+++ bookpage.webui/ViewModels/ProductViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using bookpage.entity;
+
+namespace bookpage.webui.ViewModels
+{
+    public class ProductViewModel
+    {
+        public List<Product> Products { get; set; }
+    }
+}
